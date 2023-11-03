c4f99c349a0484e63b97cf175039ff44bf63b0a3
Ryan Lee <ryanlee7171@gmail.com>
2023-11-03 19:39:53 -0400

    Reverted compute embedding time python file back to older version

--- compute_embedding_times.py
+++ compute_embedding_times.py
@@ -15,8 +15,8 @@ from truncated_SpectralEmbedding import *
 root_dir = "/home/ryanlee/Repositories/AISTATS_2024"
 #########################################################################################################################
 
-#dataset_name = ["Cora", "CiteSeer", "PubMed", "WikiCs", "Arxiv", "Products"] # dataset name
-dataset_name = ["Products"]
+dataset_name = ["Cora", "CiteSeer", "PubMed", "WikiCs", "Arxiv", "Products"] # dataset name
+
 ######################################################################################################################### results df
 torch.cuda.empty_cache()
 gc.collect()
